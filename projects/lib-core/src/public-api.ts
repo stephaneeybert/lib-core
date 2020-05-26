d9f17c1762e849063d1e916b75876ba4b6711744
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-26 16:34:25 +0200

    Added a download service to the library.

--- projects/lib-core/src/public-api.ts
+++ projects/lib-core/src/public-api.ts
@@ -9,4 +9,5 @@ export * from './lib/service/local-storage.service';
 export * from './lib/service/notification.service';
 export * from './lib/service/screen-device.service';
 export * from './lib/service/wakelock.service';
+export * from './lib/service/download.service';
 export * from './lib/directive/click-outside.directive';
