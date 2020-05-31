90df0dee73eca3889837216c052a789a418e5917
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-31 15:00:15 +0200

    Added missing semi-colons.

--- projects/lib-core/src/lib/download/download.ts
+++ projects/lib-core/src/lib/download/download.ts
@@ -1,7 +1,7 @@
 export interface Download {
 
-  state: 'PENDING' | 'IN_PROGRESS' | 'DONE'
-  progress: number
-  content: Blob | null
+  state: 'PENDING' | 'IN_PROGRESS' | 'DONE';
+  progress: number;
+  content: Blob | null;
 
 }
