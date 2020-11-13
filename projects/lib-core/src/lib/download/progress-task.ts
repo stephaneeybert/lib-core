3f7b67c23148ffc8a76e57bb77f1ee1a354ea46a
Stephane Eybert <mittiprovence@yahoo.se>
2020-11-13 18:25:43 +0100

    Beautified the code.

--- projects/lib-core/src/lib/download/progress-task.ts
+++ projects/lib-core/src/lib/download/progress-task.ts
@@ -1,12 +1,12 @@
 export class ProgressTask<T> {
 
-  total: number;
-  loaded: number;
   body: T | null;
+  loaded: number;
+  total: number;
 
   constructor(total: number, loaded: number, body?: T) {
-    this.total = total;
     this.loaded = loaded;
+    this.total = total;
     if (body) {
       this.body = body;
     }
